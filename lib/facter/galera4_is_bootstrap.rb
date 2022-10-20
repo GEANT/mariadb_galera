6029694ee5ea889ef3fc79e4dc8b40b677ec1d8d
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-20 21:50:01 +0200

    remove useless facts

--- lib/facter/galera4_is_bootstrap.rb
+++
@@ -1,9 +0,0 @@
-Facter.add(:galera4_is_bootstrap) do
-  setcode do
-    if system('/usr/bin/systemctl status mysql@bootstrap >/dev/null 2>&1')
-      true
-    else
-      false
-    end
-  end
-end
--- lib/facter/galera4_rootcnf_exist.rb
+++
@@ -1,9 +0,0 @@
-Facter.add('galera4_rootcnf_exist') do
-  setcode do
-    if File.file?('/root/.my.cnf')
-      true
-    else
-      false
-    end
-  end
-end
