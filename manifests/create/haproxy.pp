b4303db1e644751e56616985d2545b529b445049
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-22 01:27:13 +0200

    tpo fix

--- manifests/create/haproxy.pp
+++ manifests/create/haproxy.pp
@@ -7,7 +7,7 @@ class mariadb_galera::create::haproxy {
   $sql_file = "CREATE USER haproxy@'%';\nGRANT PROCESS ON *.* TO 'haproxy'@'%';\nFLUSH PRIVILEGES;\n"
 
   exec { 'create-haproxy-user':
-    command   => "mysql mysql --defaults-file=/root/.my.cnf -e \"${sql_file}\"",
+    command   => "mysql --defaults-file=/root/.my.cnf -e \"${sql_file}\"",
     unless    => 'mysql --no-defaults -u haproxy -h localhost -Be "select 1 from dual"',
     path      => '/bin:/sbin',
     logoutput => true,
