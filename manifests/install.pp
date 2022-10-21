31a84da074dda84e600952a735ab4c8b55b1c59a
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-22 00:30:05 +0200

    code clean-up

--- manifests/install.pp
+++ manifests/install.pp
@@ -22,6 +22,7 @@ class mariadb_galera::install (
     package_name       => 'mariadb-server',
     package_ensure     => present,
     service_name       => 'mariadb',
+    managed_dirs       => false,
     manage_config_file => false,
     require            => [Exec['apt_update'], Apt::Source['mariadb-server']];
   }
