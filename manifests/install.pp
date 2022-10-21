27d115cd92387cb5a71ff6f202b7aafa8fe307de
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-21 23:23:36 +0200

    minot

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,13 +5,13 @@ class mariadb_galera::install (
   $mariadb_packages = $mariadb_galera::params::mariadb_packages
 ) {
 
-  #package { 'mysql_client':
-  #  ensure  => present,
-  #  name    => 'mariadb',
-  #  require => [Exec['apt_update'], Apt::Source['mariadb-server']];
-  #  #provider        => $mysql::client::package_provider,
-  #  #source          => $mysql::client::package_source,
-  #}
+  package { 'mysql_client':
+    ensure  => present,
+    name    => 'mariadb',
+    require => [Exec['apt_update'], Apt::Source['mariadb-server']];
+    #provider        => $mysql::client::package_provider,
+    #source          => $mysql::client::package_source,
+  }
 
   package { 'mysql-server':
     ensure  => present,
