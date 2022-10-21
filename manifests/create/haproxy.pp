d806af117811c6acee5c41a996ec580b6a6a9ebc
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-22 01:00:48 +0200

    logoutput

--- manifests/create/haproxy.pp
+++ manifests/create/haproxy.pp
@@ -6,10 +6,11 @@ class mariadb_galera::create::haproxy {
   $sql_file = "CREATE USER haproxy@'%';\nGRANT PROCESS ON *.* TO 'haproxy'@'%';\nFLUSH PRIVILEGES;\n"
 
   exec { 'create-haproxy-user':
-    command => "mysql -Be \"${sql_file}\"",
-    unless  => 'mysql -u haproxy -h localhost -e "select 1 from dual"',
-    path    => '/bin:/sbin',
-    require => Service['mariadb'];
+    command   => "mysql -Be \"${sql_file}\"",
+    unless    => 'mysql -u haproxy -h localhost -e "select 1 from dual"',
+    path      => '/bin:/sbin',
+    logoutput => true,
+    require   => Service['mariadb'];
   }
 
 }
