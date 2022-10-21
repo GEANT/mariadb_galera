39de5a23444d2b7f702a1ac7be132b265e036da2
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-22 01:07:32 +0200

    minor

--- manifests/create/haproxy.pp
+++ manifests/create/haproxy.pp
@@ -8,7 +8,7 @@ class mariadb_galera::create::haproxy {
   $sql_file = "CREATE USER haproxy@'%';\nGRANT PROCESS ON *.* TO 'haproxy'@'%';\nFLUSH PRIVILEGES;\n"
 
   exec { 'create-haproxy-user':
-    command   => "mysql -Be \"${sql_file}\"",
+    command   => "mysql -e \"${sql_file}\"",
     unless    => 'mysql -u haproxy -h localhost -e "select 1 from dual"',
     path      => '/bin:/sbin',
     logoutput => true,
