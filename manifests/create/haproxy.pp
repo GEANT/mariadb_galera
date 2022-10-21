584309dd73bb3a16221f567904bbc76a2493ed58
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-22 00:48:45 +0200

    fix sql statement

--- manifests/create/haproxy.pp
+++ manifests/create/haproxy.pp
@@ -6,7 +6,7 @@ class mariadb_galera::create::haproxy {
   $sql_file = "CREATE USER haproxy@'%';\nGRANT PROCESS ON *.* TO 'haproxy'@'%';\nFLUSH PRIVILEGES;\n"
 
   exec { 'create-haproxy-user':
-    command => "mysql < ${sql_file}",
+    command => "mysql -Be ${sql_file}",
     unless  => 'mysql -u haproxy -h localhost -e "select 1 from dual"',
     path    => '/bin:/sbin',
     require => Service['mariadb'];
