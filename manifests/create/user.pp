e08a924dedc286a33ffe8a5df18a11e62605ae1a
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-21 11:59:39 +0200

    fixing user creation

--- manifests/create/user.pp
+++ manifests/create/user.pp
@@ -3,15 +3,17 @@
 #
 define galera_proxysql::create::user (
   Sensitive $dbpass,
-  $galera_hosts,
+  $galera_ipv4,
+  $galera_ipv6                  = [],
   $privileges                   = ['SELECT'],
   Variant[Array, String] $table = '*.*',  # Example: 'schema.table', 'schema.*', '*.*'
   $dbuser                       = $name,
   $force_schema_removal         = false,  # do not drop DB if a user is removed
-  Enum[
-    'present', 'absent',
-    present, absent] $ensure    = present
+  Enum['present', 'absent', present, absent] $ensure = present,
+  Array[Stdlib::IP::Address, Stdlib::Fqdn] $trusted_sources = []
 ) {
+
+  $galera_ips = $galera_ipv4 + $galera_ipv6
 
   if $table =~ String {
     $schema_name = split($table, '[.]')[0]
@@ -25,9 +27,18 @@ define galera_proxysql::create::user (
     $ensure_schema = present
   }
 
-  if $schema_name =~ String {
-    unless defined(Mysql::Db[$schema_name]) {
-      mysql::db { $schema_name:
+  if $schema_name =~ Array {
+    $schema_array = $schema_name
+  } else {
+    $schema_array = [$schema_name]
+  }
+
+  # start '*' is not a schema that we need to create
+  $schema_array_no_stars = $schema_array.filter |$item| { $item =~ Undef }
+
+  $schema_array_no_stars.each | $myschema | {
+    unless defined(Mysql::Db[$myschema]) {
+      mysql::db { $myschema:
         ensure   => $ensure_schema,
         user     => $dbuser,
         password => $dbpass.unwrap,
@@ -36,39 +47,36 @@ define galera_proxysql::create::user (
         collate  => 'utf8_bin';
       }
     }
-  } else {
-    $schema_name.each | $myschema | {
-      unless defined(Mysql::Db[$myschema]) {
-        mysql::db { $myschema:
-          ensure   => $ensure_schema,
-          user     => $dbuser,
-          password => $dbpass.unwrap,
-          grant    => $privileges,
-          charset  => 'utf8',
-          collate  => 'utf8_bin';
-        }
-      }
+  }
+
+  $galera_ips.each | $galera_ip | {
+    mysql_user { "${dbuser}@${galera_ip}":
+      ensure        => $ensure,
+      password_hash => mysql_password($dbpass.unwrap),
+      provider      => 'mysql',
+      require       => Mysql::Db[$schema_array_no_stars];
     }
   }
-  $galera_hosts.each | $host_name, $host_ips | {
-    mysql_user {
-      "${dbuser}@${host_ips['ipv4']}":
-        ensure        => $ensure,
-        password_hash => mysql_password($dbpass.unwrap),
-        provider      => 'mysql',
-        require       => Mysql::Db[$schema_name];
-      "${dbuser}@${host_name}":
-        ensure        => $ensure,
-        password_hash => mysql_password($dbpass.unwrap),
-        provider      => 'mysql',
-        require       => Mysql::Db[$schema_name];
+
+  unless trusted_sources == [] {
+
+    $_translated_trusted_sources = $trusted_sources.map |$item| {
+      if item =~ Stdlib::Fqdn {
+        [dns_a($item)[0], downcase(dns_aaaa($item)[0])]
+      } elsif item =~ Stdlib::IP::Address::V6 { downcase(dns_aaaa($item)[0])
+      } else {
+        dns_a($item)[0]
+      }
     }
-    if has_key($host_ips, 'ipv6') {
-      mysql_user { "${dbuser}@${host_ips['ipv6']}":
+
+    $translated_trusted_sources = unique(flatte($_translated_trusted_sources))
+
+    $translated_trusted_sources.each | $item | {
+      mysql_user { "${dbuser}@${item}":
         ensure        => $ensure,
         password_hash => mysql_password($dbpass.unwrap),
         provider      => 'mysql',
-        require       => Mysql::Db[$schema_name];
+        require       => Mysql::Db[$schema_array_no_stars];
       }
     }
   }
