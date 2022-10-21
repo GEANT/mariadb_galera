82409c548c1a06166fd3f13cf7221e1539c14c9a
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-22 00:14:07 +0200

    use puppetlabs module

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,23 +5,30 @@ class mariadb_galera::install (
   $mariadb_packages = $mariadb_galera::params::mariadb_packages
 ) {
 
-  class { 'mysql::client':
-    package_manage  => false,
-    bindings_enable => false
-  }
-  class { 'mysql::server':
-    package_manage  => false,
-  }
-
   #class { 'mysql::client':
-  #  package_name    => 'mariadb-client',
-  #  package_ensure  => present,
-  #  bindings_enable => false,
+  #  package_manage  => false,
+  #  bindings_enable => false
+  #}
+  #class { 'mysql::server':
+  #  package_manage  => false,
   #}
 
-  package { ['mariadb-client', 'mysql-server']:
-    ensure  => present,
-    require => [Exec['apt_update'], Apt::Source['mariadb-server']];
+  class { 'mysql::client':
+    package_name    => 'mariadb-client',
+    package_ensure  => present,
+    bindings_enable => false,
   }
+  class { 'mysql::server':
+    package_name       => 'mariadb-server',
+    package_ensure     => present,
+    service_name       => 'mariadb',
+    manage_config_file => false,
+    require            => [Exec['apt_update'], Apt::Source['mariadb-server']];
+  }
+
+  #package { ['mariadb-client', 'mysql-server']:
+  #  ensure  => present,
+  #  require => [Exec['apt_update'], Apt::Source['mariadb-server']];
+  #}
 
 }
