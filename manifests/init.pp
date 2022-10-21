1a05d37feaa4928f0bc114482b7c5b58d0e57174
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-21 15:49:32 +0200

    minor

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,8 +17,9 @@ class mariadb_galera (
   }
 
   $nomad_server_hash = puppetdb_query(
-    "inventory[facts.fqdn] {facts.hostname ~ 'nomad0\d' and facts.agent_specified_environment = '${::environment}'}")
+    "inventory[facts.fqdn] {facts.hostname ~ 'nomad0' and facts.agent_specified_environment = '${::environment}'}")
   $nomad_server = sort($nomad_server_hash.map | $k, $v | {$v['facts.fqdn'] })
+
   echo { "test ${nomad_server}": }
 
 }
