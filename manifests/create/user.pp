c7f08e6d947184b50344b3f6edbf08c21df9bd7c
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-25 17:03:35 +0200

    minor

--- manifests/create/user.pp
+++ manifests/create/user.pp
@@ -73,7 +73,9 @@ define mariadb_galera::create::user (
       }
     }
 
-    $translated_trusted_sources = unique(flatten($_translated_trusted_sources))
+    $_translated_trusted_sources = unique(flatten($_translated_trusted_sources))
+
+    $translated_trusted_sources = $_translated_trusted_sources.filter |$val| { $val =~ NotUndef }
 
     $translated_trusted_sources.each | $item | { echo { $item: } }
 
