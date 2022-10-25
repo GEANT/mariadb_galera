a0959dd4ded3f8dfd43bb0cc993ea97b50fd3a25
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-25 23:09:16 +0200

    minor

--- manifests/create/grant.pp
+++ manifests/create/grant.pp
@@ -17,9 +17,9 @@ define mariadb_galera::create::grant (
     $table_array = $table
   }
 
+  $down_source = downcase($source)
+
   $table_array.each | $table_item | {
-    $down_source = downcase($source)
-    echo { "${dbuser}@${down_source}/${table_item}": }
     mysql_grant { "${dbuser}@${down_source}/${table_item}":
       ensure     => $ensure,
       user       => "${dbuser}@${down_source}",
