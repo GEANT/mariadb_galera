b42fe63796fadcc42d906d6010ad6f48e26bc7cd
Massimiliano Adamo <maxadamo@gmail.com>
2022-10-25 16:54:03 +0200

    add debug

--- manifests/create/user.pp
+++ manifests/create/user.pp
@@ -64,14 +64,13 @@ define mariadb_galera::create::user (
   }
 
   unless trusted_sources == [] {
-
+    $_translated_trusted_sources.each | $item | { echo { $item } }
     $_translated_trusted_sources = $trusted_sources.map |$item| {
       if item =~ Stdlib::Fqdn {
         [dns_a($item)[0], dns_aaaa($item)[0]]
       } else {
         $item
       }
-      echo { $item: }
     }
 
     $translated_trusted_sources = unique(flatten($_translated_trusted_sources))
