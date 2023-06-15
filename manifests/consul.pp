ce4296e7daadcd8cabe8351c6954e736d1437118
Massimiliano Adamo <maxadamo@gmail.com>
2023-06-15 18:20:54 +0200

    minor

--- manifests/consul.pp
+++ manifests/consul.pp
@@ -26,7 +26,7 @@ class mariadb_galera::consul (String $consul_service_name) {
     tags    => [
       "${db_env}-traefik.enable=true",
       "${db_env}-traefik.tcp.routers.mariadb.rule=HostSNI(`*`)",
-      "${db_env}-traefik.tcp.routers.mariadb.entrypoints=${db_env}-mariadb-galera",
+      "${db_env}-traefik.tcp.routers.mariadb.entrypoints=mariadb-galera",
       "${db_env}-traefik.tcp.routers.mariadb.service=${db_env}-mariadb-galera",
       "${db_env}-traefik.tcp.services.mariadb.loadbalancer.server.port=3306",
     ],
